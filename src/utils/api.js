54904f040e4c746d210f7ca398fa66ef6505947f
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-03-04 21:06:08 +0530

    api call code

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,13 @@
 import axios from "axios";
+const BASE_URL = "https://www.googleapis.com/customsearch/v1";
+const params = {
+  key: "AIzaSyDW8Lul1dJPfgO2eCcX3_QJAolWyKiARXE",
+  cx: "35c1e05be33b64132",
+};
 
-export const fetchDataFromApi = () => {};
+export const fetchDataFromApi = async (payload) => {
+  const { data } = await axios.get(BASE_URL, {
+    params: { ...params, ...payload },
+  });
+  return data;
+};

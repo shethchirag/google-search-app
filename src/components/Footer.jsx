266e35bf2185dbbd1b6664315cc2020869180760
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-03-05 17:17:54 +0530

    error resolve

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { quickLinks, settingMenu } from "../utils/constants";
+import { quickLinks, settingMenu } from "../utils/Constants";
 
 const Footer = () => {
   return (

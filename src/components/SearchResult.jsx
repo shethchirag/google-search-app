93c06ce22339a41ac7d8aa82d7c042c26ecee641
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-03-05 17:14:42 +0530

    setup pagination

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -27,10 +27,33 @@ const SearchResult = () => {
       setResult(res);
     });
   };
+
+  if (!result) return;
+  const { items, queries, searchInformation } = result;
   return (
     <div className="flex flex-col min-h-[100vh]">
       <SearchResultHeader />
-      <main className="grow p-[12px] pb-0 md:pr-5 md:pl-20"></main>
+      <main className="grow p-[12px] pb-0 md:pr-5 md:pl-20">
+        <div className="flex text-sm text-[#70757a] mb-3">
+          {`About ${searchInformation.formattedTotalResults} results in (${searchInformation.formattedSearchTime})`}
+        </div>
+        {!imageSearch ? (
+          <>
+            {items.map((item, index) => (
+              <SearchedItemTemplate key={index} data={item} />
+            ))}
+          </>
+        ) : (
+          <>
+            <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-4">
+              {items.map((item, index) => (
+                <SearchedImageItemTemplate key={index} data={item} />
+              ))}
+            </div>
+          </>
+        )}
+      </main>
+      <Pagination queries={queries} />
       <Footer />
     </div>
   );
